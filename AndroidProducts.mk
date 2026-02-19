5e8adf8e077ffc24cc5a4bf518b5e74c5017e443
ShadowbytePrjkt <chesstype438@gmail.com>
2026-02-19 18:19:21 +0800

    twrp to pbrp

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_X6886.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_X6886-eng \
+    pb_X6886-userdebug \
+    pb_X6886-user
