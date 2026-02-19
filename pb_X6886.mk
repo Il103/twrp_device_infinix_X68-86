315fc57515df17766c0a53c2a9f527eaba663836
ShadowbytePrjkt <chesstype438@gmail.com>
2026-02-19 18:19:16 +0800

    twrp to pbrp

---
+++ pb_X6886.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from Infinix-X6886 device
+$(call inherit-product, device/infinix/X6886/device.mk)
+
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := pb_X6886
+PRODUCT_DEVICE := X6886
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6886
+PRODUCT_MANUFACTURER := INFINIX
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
